43ed26b473f7494acc8dbdb079aee10033b53249
almighty.yantao <yantao@qunhemail.com>
2024-02-23 18:17:33 +0800

    Initial Commit

---
+++ Msss/MsssApp.swift
@@ -0,0 +1,17 @@
+//
+//  MsssApp.swift
+//  Msss
+//
+//  Created by zz-yt on 23.2.24.
+//
+
+import SwiftUI
+
+@main
+struct MsssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
